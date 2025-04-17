50b40574ed0522df7cda7805c25e9bfc39ee45c2
Antonio Vurro <antoniovurro@MacBook-Pro-di-Antonio-2.local>
2025-04-17 17:04:26 +0200

    Usati i path corretti per i CSS in src/styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,42 @@
 // src/App.jsx
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Login';
-import GridDashboard from './GridDashboard';
-import BoxDetails from './BoxDetails';
-import ProjectDesign from './pages/ProjectDesign.jsx'; // Usa l'estensione
 
-function App() {
+import Login              from './Login';
+import BoxDetails         from './BoxDetails';
+
+import MagazzinoView            from './pages/MagazzinoView';
+import MagazzinoDesign          from './pages/MagazzinoDesign';
+import AssettoNazionale         from './pages/AssettoNazionale';
+import AssettoNazionaleDesign   from './pages/AssettoNazionaleDesign';
+import AssettoInternazionale    from './pages/AssettoInternazionale';
+import AssettoInternazionaleDesign from './pages/AssettoInternazionaleDesign';
+
+export default function App() {
   const [token, setToken] = useState(localStorage.getItem('token') || null);
-
-  const handleLogin = (newToken) => {
-    localStorage.setItem('token', newToken);
-    setToken(newToken);
+  const handleLogin = (t) => {
+    localStorage.setItem('token', t);
+    setToken(t);
   };
 
   return (
     <BrowserRouter>
       <Routes>
-        {!token ? (
-          <Route path="*" element={<Login onLogin={handleLogin} />} />
-        ) : (
-          <>
-            <Route path="/" element={<GridDashboard token={token} />} />
-            <Route path="/cassa/:id" element={<BoxDetails token={token} />} />
-            <Route path="/progettazione" element={<ProjectDesign token={token} />} />
-          </>
-        )}
+        {!token
+          ? <Route path="*" element={<Login onLogin={handleLogin} />} />
+          : <>
+              <Route path="/" element={<MagazzinoView token={token} />} />
+              <Route path="/magazzino/design" element={<MagazzinoDesign token={token} />} />
+
+              <Route path="/assetto-nazionale" element={<AssettoNazionale token={token} />} />
+              <Route path="/assetto-nazionale/design" element={<AssettoNazionaleDesign token={token} />} />
+
+              <Route path="/assetto-internazionale" element={<AssettoInternazionale token={token} />} />
+              <Route path="/assetto-internazionale/design" element={<AssettoInternazionaleDesign token={token} />} />
+
+              <Route path="/cassa/:id" element={<BoxDetails token={token} />} />
+            </> }
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
